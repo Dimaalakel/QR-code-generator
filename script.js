d67b63a8631bbdc9d3551ecc8264fdbd51c02bf4
Dima1 <dimaalakel1@gmail.com>
2024-04-21 03:17:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let QRimage = document.getElementById("QRimage"); 
+let imgbox = document.getElementById("imgbox");
+let QRtext = document.getElementById("QRtext");
+
+function generateQr(){
+    if(QRtext.value.length > 0){
+        QRimage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRtext.value;
+imgbox.classList.add("show-img")
+    }else{
+        QRtext.classList.add('error');
+        setTimeout(()=>{
+            QRtext.classList.remove('error'); 
+        },1000)
+    }
+    
+}
